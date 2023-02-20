00d06ec36d50fd8b793eed8c66172ecd42c41589
Ursulo Cabeza <ursuloacabezad@gmail.com>
2023-02-20 12:01:04 -0400

    terminando desktop fm-18

--- fm-18/tailwind.config.js
+++ fm-18/tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {
+      backgroundImage: {
+        "footer-desktop-top": "url('../images/bg-footer-top-desktop.svg')",
+        "footer-mobile-top": "url('../images/bg-footer-top-mobile.svg')",
+        "bg-section-bottom-desktop-1": "url('/img/footer-texture.png')",
+        "bg-section-bottom-desktop-2": "url('/img/footer-texture.png')",
+        "bg-section-bottom-mobile-1": "url('/img/footer-texture.png')",
+        "bg-section-bottom-mobile-2": "url('/img/footer-texture.png')",
+        "bg-section-top-desktop-1": "url('/img/footer-texture.png')",
+        "bg-section-top-desktop-2": "url('/img/footer-texture.png')",
+        "bg-section-top-mobile-1": "url('/img/footer-texture.png')",
+        "bg-section-top-mobile-2": "url('/img/footer-texture.png')",
+      },
       colors: {
         "Color-pink": "#ff52bf",
         "Light-Pink": "#ff8fd8",
